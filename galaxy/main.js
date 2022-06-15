a4e3559bf2afbb30b6457d03e1655395eb08b13c
renbowang <rencool1031@gmail.com>
2022-06-15 12:17:11 +0800

    First commit

---
+++ galaxy/main.js
@@ -0,0 +1,60 @@
+let pageData = {
+    productClasses:[ {
+        name:'Coach'
+        ,
+        price:125000
+        ,
+        seatsAvailable:20
+        ,
+        earlyBird:true
+        },{
+        name:'Business'
+        ,
+        price:275000
+        ,
+        seatsAvailable:
+        6
+        ,
+        earlyBird:true
+        },{
+        name:'First'
+        ,
+        price:430000
+        ,
+        seatsAvailable:
+        2
+        ,
+        earlyBird:false
+        }
+        ],
+        
+    productName: 'Book a Cruise to the Moon',
+    productDescription: 'Cruise to the moon in our luxurious shuttle. Watch the astronauts working outside the International Space Station.',
+    imageSrc:[
+        "asteroid.jpg",
+        "fantasy.jpg",
+        "space.jpg",
+        "spaceship.jpg"
+        ],h2ClassController:{
+            centered:true,
+            colorFont:false
+            },pStyleController:{
+                'margin-left':'50px',
+                color:'blue',
+                'font-size':'20px',
+                'font-style':'italic'
+                },imageStyleController:{
+                    margin:'auto',
+                    display:'block',
+                    width:'50%'
+                    },
+                    imageAlt:"Photo from space"
+        
+    };
+    const app = Vue.createApp({
+    data(){
+    return pageData;
+    }
+    });
+    app.mount("#app");
+    
---
+++ randomselector/main.js
@@ -0,0 +1,9 @@
+$(function () {
+    $("input").on("click", function () {
+        //alert("Hi");
+        var numberOfListItem = $("li").length;
+        var randomChildNumber = Math.floor(Math.random() * numberOfListItem);
+        $("h1").text($("li").eq(randomChildNumber).text());
+        document.getElementById("img").src = randomChildNumber + ".jpg";
+    });
+});
